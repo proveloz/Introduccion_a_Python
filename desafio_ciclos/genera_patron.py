6ac467de3c4478b40578d35464c3988aea6cde2b
pveloz <pveloz@drizip.com>
2020-11-25 15:54:52 -0300

    Create genera_patron.py

---
+++ desafio_ciclos/genera_patron.py
@@ -0,0 +1,12 @@
+n = int(input("Ingrese un número para comenzar la cuenta\n"))
+contador = 1
+j=1
+secuencia = str(1)
+while j <=n:
+    print(secuencia)
+    contador = contador +1
+    secuencia = secuencia +str(contador)
+    j = j+1
+
+
+        
